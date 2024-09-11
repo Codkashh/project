01ead07a4dcefc8610ea2d69cb253b07c958d6ce
Codkashh <as3733542@gmail.com>
2024-09-11 18:54:15 +0530

    updations of code

--- src/app.js
+++ src/app.js
@@ -10,6 +10,16 @@ app.use(cors(
     }
 ))
 
-app.use(express.json({}))
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// routes
+
+import useRouter from "./routes/user.routes"
+//routes declartion--
+app.use("/api/v1/users", useRouter)
+
 
 export { app }
